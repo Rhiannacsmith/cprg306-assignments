11deb3b9b2065f5d80377184f54b230660080f61
R.Smith <rhiannacsmith@hotmail.com>
2024-01-23 14:53:39 -0700

    Added final steps for Assign 2

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function page() {
-  return <main> Hello World! bahhh </main>;
+  return (<main> 
+    <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <Link href="week-2">Week 2</Link> 
+     </main>
+     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() 
+{
+    return (
+        <main>
+            <h1>Rhianna Smith</h1>
+            <Link href="https://github.com/Rhiannacsmith/cprg306-assignments">https://github.com/Rhiannacsmith/cprg306-assignments</Link> 
+        </main> 
+    )
+}
