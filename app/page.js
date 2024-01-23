e0dbb9bb0de9daf4915718bf7821795584e6b69e
R.Smith <rhiannacsmith@hotmail.com>
2024-01-23 15:15:01 -0700

    Added List for Rootpage

--- app/page.js
+++ app/page.js
@@ -3,7 +3,13 @@ import Link from "next/link";
 export default function page() {
   return (<main> 
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href="week-2">Week 2</Link> 
+    <ul>
+      <li>
+        <Link href="week-2">Week 2</Link> 
+      </li>
+    </ul>
+
+
      </main>
      );
 }
