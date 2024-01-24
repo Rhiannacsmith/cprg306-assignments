741a83f3ba8af4d03b233b74e9840a4ed91664ca
R.Smith <rhiannacsmith@hotmail.com>
2024-01-23 18:00:24 -0700

    Added shopping list and week 3 folder

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
 
 export default function page() {
-  return (<main> 
+  return (
+  <main> 
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
     <ul>
       <li>
         <Link href="week-2">Week 2</Link> 
       </li>
+      <li>
+        <Link href="week-3">Week 3</Link> 
+      </li>
     </ul>
-
-
-     </main>
-     );
+  </main>
+  );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import ItemList from './item-list.js'
+
+export default function Page() {
+    return(
+        <main className=" bg-blue-950">
+            <h1 className=" text-3xl font-bold text-blue-200 pl-4 pt-2">Shopping List</h1>   
+        <ItemList/>
+    
+        </main>
+    )
+}
