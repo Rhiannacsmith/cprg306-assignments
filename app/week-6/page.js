7f4a1ea0cdfcef4008f0d1e77308ba17319181de
R.Smith <rhiannacsmith@hotmail.com>
2024-03-03 18:37:36 -0700

    small change

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -16,12 +16,10 @@ export default function Page() {
     };
 
     return(
-       <div className='justify-content content-end'>
         <main className=" bg-blue-950">
          <NewItem onAddItem={handleAddItem}/> 
          <h1 className=" text-3xl font-bold text-blue-200 pl-4 pt-2">Shopping List</h1>  
          <ItemList items={items}/>
         </main>
-        </div>
     )
 }
